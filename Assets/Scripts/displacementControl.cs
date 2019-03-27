53a52fc33d1e08cd9bb2ad56a55fe17c150f2fb4
Nicholas Ramsay <nicholasramsay@hotmail.com>
2019-03-27 03:36:57 -0700

    tested some displacement shaders

---
+++ Assets/Scripts/displacementControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displacementControl : MonoBehaviour {
+
+    public float displacementAmount;
+    public ParticleSystem explosionParticles;
+    MeshRenderer meshRenderer;
+	// Use this for initialization
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRenderer.material.SetFloat("_Amount", displacementAmount);
+
+        if (Input.GetButtonDown("Jump")) {
+            displacementAmount += 0.5f;
+            explosionParticles.Play();
+        }
+		
+	}
+}
